2b1f6fe9a6b7f2cdfd77f0520ab4789bcb180b79
Connor Barbaccia <connorbarbaccia@gmail.com>
2024-01-30 12:01:12 -0600

    .

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ app.get('/linkedinaccount', (req, res) => {
   res.sendFile(__dirname + '/pages/linkedinaccount.html');
 });
 app.post('/linkedinaccount', (req, res) => {
-  let linkedIn=req.body.linkedin;
+  let linkedin=req.body.linkedin;
   let discordId=session.discordId||res.send('no discord id');
   Entry.findOne({ discordId: discordId })
   .then(async entry => {
